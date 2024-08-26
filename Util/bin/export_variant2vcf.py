c14837597719ef1e1eb44e59c1213ab595794abb
fossilfriend <egreenfest@gmail.com>
2024-08-26 14:24:57 -0400

    remove duplicate check

--- Util/bin/export_variant2vcf.py
+++ Util/bin/export_variant2vcf.py
@@ -15,18 +15,18 @@ from psycopg2 import DatabaseError
 
 from niagads.utils.string import xstr
 from niagads.utils.dict import print_dict
-from niagads.utils.sys import warning, print_args
+from niagads.utils.sys import warning, print_args, execute_cmd
 from niagads.utils.list import qw
 from niagads.utils.reg_ex import matches
 from niagads.db.postgres import Database
 
 from AnnotatedVDB.Util.enums import HumanChromosome as Human
 
-SELECT_SQL = "SELECT record_primary_key, metaseq_id FROM AnnotatedVDB.Variant WHERE chromosome = %s"
+SELECT_SQL = "SELECT record_primary_key, metaseq_id, row_algorithm_id FROM AnnotatedVDB.Variant WHERE chromosome = %s"
 VARIANTS_PER_FILE = 10000000
 ITERATION_SIZE = 500000
 VCF_HEADER = qw('#CHRM POS ID REF ALT QUAL FILTER INFO', returnTuple=False)
-INVALID_ALLELES = 'I|R|D|N'
+INVALID_ALLELES = 'I|R|D|N' # there are some "N's" in the DB, but the appear to all come from dbSNP directly
 
 def print_file(variants, directory, chromosome, count):
     vcfFile = path.join(directory, chromosome + '_' + xstr(count) + '.vcf')
@@ -39,9 +39,11 @@ def print_file(variants, directory, chromosome, count):
 
 def run(directory:str, chromosome:str):
     logFileName = path.join(directory, chromosome + ".log")
+    invalidFileName = path.join(directory, chromosome + "_invalid.txt")
+    # duplicateFileName = path.join(directory, chromosome + "_duplicates.txt")
     warning("Starting " + chromosome + "; logging to " + logFileName)
-    
-    reference = {}
+
+    duplicates = {}
     try:
         database = Database(args.gusConfigFile)
         database.connect()
@@ -49,42 +51,51 @@ def run(directory:str, chromosome:str):
         
         fileCount = 1
         with database.named_cursor(cname, cursorFactory="RealDictCursor") as selectCursor, \
-            open(logFileName, 'w') as lfh: 
+            open(logFileName, 'w') as lfh, \
+            open(invalidFileName, 'w') as ifh:
+            # open(duplicateFileName, 'w') as dfh, \
 
             warning("Processing " + chromosome, file=lfh, flush=True)
             warning("Executing query: " + SELECT_SQL.replace('%s', "'" + chromosome + "'"), file=lfh, flush=True)
             
             selectCursor.itersize = 500000 
             selectCursor.execute(SELECT_SQL, [chromosome])
-            recordCount = 0
+            validCount = 0
             variants = {}
             for record in selectCursor:     
-                recordCount += 1
                 primaryKey = record['record_primary_key']
                 metaseqId = record['metaseq_id']
+                rowAlgId = record['row_algorithm_id']
+                
+                """ it appears all (hopefully) duplicates are invalid variants or from an early NHGRI GWAS Catalog Load
+                if primaryKey in variants or duplicates.get(primaryKey) != True: # not in the hash or False
+                    print(primaryKey, rowAlgId, sep='\t', file=dfh, flush=True)
+                    duplicates[primaryKey] = True
+                    continue
+                """
                 
                 if matches(INVALID_ALLELES, metaseqId):
-                    warning("INVALID: " + primaryKey, file=lfh, flush=True)
-                    continue
-                
-                if primaryKey in reference:
-                    warning("DUPLICATE: " + primaryKey, file=lfh, flush=True)
+                    print(primaryKey, rowAlgId, sep='\t', file=ifh, flush=True)
                     continue
                 else:
-                    reference[primaryKey] = True
+                    validCount += 1
                     variants[primaryKey] = metaseqId
                 
-                if recordCount % ITERATION_SIZE == 0:
-                    warning("Fetched " + xstr(recordCount) + " records from " + chromosome + ".", file=lfh, flush=True)
+                if validCount % ITERATION_SIZE == 0:
+                    warning("Fetched " + xstr(validCount) + " valid records from " + chromosome + ".", file=lfh, flush=True)
                     
-                if recordCount % VARIANTS_PER_FILE == 0:
-                    warning("Fetched " + xstr(recordCount) + " records from " + chromosome + ". Writing to file " + xstr(fileCount), file=lfh, flush=True)
+                if validCount % VARIANTS_PER_FILE == 0:
+                    warning("Fetched " + xstr(validCount) + " records from " + chromosome + ". Writing to file " + xstr(fileCount), file=lfh, flush=True)
                     print_file(variants, directory, chromosome, fileCount)
+                    
+                    # del duplicates
+                    # duplicates = { k: False for k in variants.keys()}   
+
                     variants = {}
                     fileCount += 1
             
             # residuals
-            warning("Fetched " + xstr(recordCount) + " records from " + chromosome + ". Writing residuals to file " + xstr(fileCount), file=lfh, flush=True)
+            warning("Fetched " + xstr(validCount) + " valid records from " + chromosome + ". Writing residuals to file " + xstr(fileCount), file=lfh, flush=True)
             print_file(variants, directory, chromosome, fileCount)
                 
     except Exception as err:
